43937371c219517041768a0002b22e1d8acc52c5
raj-subhankar <subhankar.rj@gmail.com>
2020-12-24 12:52:38 +0530

    Implement Sidebar header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Imessage from './Imessage';
+import './Imessage.css'
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build iMessage clone</h1>
+      <Imessage />
     </div>
   );
 }
---
+++ src/Imessage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+function Imessage () {
+    return(
+        <div className="imessage">
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Imessage
